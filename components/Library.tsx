ca6d5543431953771ae77377cb46eb2d5bc6dbf7
garydsong <ceouls@gmail.com>
2023-09-28 12:11:53 -0700

    no auth for play

--- components/Library.tsx
+++ components/Library.tsx
@@ -27,18 +27,18 @@ const Library: React.FC<LibraryProps> = ({
   const onPlay = useOnPlay(songs);
 
   const onClick = () => {
-    if (!user) {
-      return authModal.onOpen();
-    }
+    // if (!user) {
+    //   return authModal.onOpen();
+    // }
 
-    if (!subscription) {
-      return subscribeModal.onOpen();
-    }
+    // if (!subscription) {
+    //   return subscribeModal.onOpen();
+    // }
 
     return uploadModal.onOpen();
   }
 
-  return ( 
+  return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
@@ -47,22 +47,22 @@ const Library: React.FC<LibraryProps> = ({
             Your Library
           </p>
         </div>
-        <AiOutlinePlus 
-          onClick={onClick} 
-          size={20} 
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
           className="
-            text-neutral-400 
-            cursor-pointer 
-            hover:text-white 
+            text-neutral-400
+            cursor-pointer
+            hover:text-white
             transition
           "
         />
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((item) => (
-          <MediaItem 
-            onClick={(id: string) => onPlay(id)} 
-            key={item.id} 
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
             data={item}
           />
         ))}
@@ -70,5 +70,5 @@ const Library: React.FC<LibraryProps> = ({
     </div>
    );
 }
- 
+
 export default Library;
